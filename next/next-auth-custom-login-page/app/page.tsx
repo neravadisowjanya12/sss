ae45330986eb6eb736c71372473d18d7aa9b6d8b
neravadisowjanya12 <154427180+neravadisowjanya12@users.noreply.github.com>
2024-01-04 12:42:20 +0530

    Add files via upload

---
+++ next/next-auth-custom-login-page/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Button from "@elements/Button";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+
+const HomePage = () => {
+  return <div>Home Page</div>;
+};
+
+export default HomePage;
